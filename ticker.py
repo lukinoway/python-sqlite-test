d98b5c44184e822a3573012d5d68f04f3f8d24f9
unknown <lukas.pichler@gmail.com>
2017-11-29 23:50:07 +0100

    added support for kraken ticker

--- ticker.py
+++ ticker.py
@@ -5,11 +5,13 @@
 ########################################
 
 from bittrex import Bittrex
+import krakenex
 import json
 import sqlite3
 import time
 
 my_bittrex = Bittrex(None, None)
+k = krakenex.API()
 
 
 # function to get bid/ask info for given market and stores the data in sqlite db
@@ -34,3 +36,29 @@ def get_ticker_for_name(market_name):
 	db.commit()
 
 	db.close
+	
+def get_kraken_ticker_for_name(market_name):
+	# try to get data from kraken servers
+	# be aware of slow and unstable servers
+	try:
+		result = k.query_public('Ticker', data = {'pair': market_name})
+		parsed_json = json.dumps(result)
+		t = json.loads(parsed_json)
+		bid = t["result"][market_name]["b"][0]
+		ask = t["result"][market_name]["a"][0]
+
+		db_location = "data/kraken_" + market_name
+		db = sqlite3.connect(db_location)
+
+		cursor = db.cursor()
+		cursor.execute("CREATE TABLE if not exists kurs(id INTEGER PRIMARY KEY, bid DECIMAL, ask DECIMAL)")
+		db.commit()
+
+		# insert data
+		cursor.execute('''INSERT INTO kurs(id, bid, ask) VALUES(?,?,?)''', (int(time.time()), bid, ask))
+		db.commit()
+
+		db.close
+		
+	except:
+		print("kraken server error")
--- ticker_manager.py
+++ ticker_manager.py
@@ -7,9 +7,17 @@
 import ticker
 
 ticker_file = open("cfg/ticker_monitor.cfg")
+kraken_file = open("cfg/kraken_ticker.cfg")
 
+# process bittrex file
 for line in ticker_file:
 	market = line.replace("\n", "") # remove new line 
 	ticker.get_ticker_for_name(market)
+	
+# process kraken file
+for line in kraken_file:
+	market = line.replace("\n", "") # remove new line 
+	ticker.get_kraken_ticker_for_name(market)
 
 ticker_file.close()
+kraken_file.close()
